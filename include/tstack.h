9faae821d9a1f0838c8812f4e087f64c00ea78dc
Vegsdo21 <sgt.pepper21@mail.ru>
2025-06-06 15:34:55 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,25 +1,33 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <stdexcept>
 
 template<typename T, int size>
 class TStack {
  private:
-  T data[size];
+  static constexpr int kStackSize = size;
+  T data[kStackSize];
   int top;
 
  public:
   TStack() : top(-1) {}
 
   void push(const T& val) {
-    if (top < size - 1) data[++top] = val;
+    if (top >= kStackSize - 1)
+      throw std::overflow_error("Stack overflow");
+    data[++top] = val;
   }
 
   T pop() {
+    if (top == -1)
+      throw std::underflow_error("Stack underflow");
     return data[top--];
   }
 
   T peek() const {
+    if (top == -1)
+      throw std::underflow_error("Stack underflow");
     return data[top];
   }
 
