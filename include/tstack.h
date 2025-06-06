64d72043ac6c361bd783646598fc4dc0ff7a55a6
Vegsdo21 <sgt.pepper21@mail.ru>
2025-06-06 15:07:40 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,35 @@
-// Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+  T data[size];
+  int top;
+
+ public:
+  TStack() : top(-1) {}
+
+  void push(const T& val) {
+    if (top < size - 1)
+      data[++top] = val;
+  }
+
+  T pop() {
+    if (!isEmpty())
+      return data[top--];
+    return T();
+  }
+
+  T peek() const {
+    if (!isEmpty())
+      return data[top];
+    return T();
+  }
+
+  bool isEmpty() const {
+    return top == -1;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
