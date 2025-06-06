1cb338ee2d20add48dec214cc84c4da900698a62
Vegsdo21 <sgt.pepper21@mail.ru>
2025-06-06 15:12:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,4 +1,3 @@
-// Copyright 2025 NNTU-CS
 #include <string>
 #include <cctype>
 #include "tstack.h"
@@ -11,29 +10,31 @@ int getPriority(char op) {
 
 std::string infx2pstfx(const std::string& inf) {
   TStack<char, 100> opStack;
-  std::string result;
+  std::string res;
   int i = 0;
 
-  while (i < inf.size()) {
+  while (i < inf.length()) {
     if (std::isdigit(inf[i])) {
-      while (i < inf.size() && std::isdigit(inf[i])) {
-        result += inf[i++];
+      while (i < inf.length() && std::isdigit(inf[i])) {
+        res += inf[i++];
       }
-      result += ' ';
+      res += ' ';
     } else if (inf[i] == '(') {
       opStack.push('(');
       i++;
     } else if (inf[i] == ')') {
       while (!opStack.isEmpty() && opStack.peek() != '(') {
-        result += opStack.pop();
-        result += ' ';
+        res += opStack.pop();
+        res += ' ';
       }
       if (!opStack.isEmpty()) opStack.pop();
       i++;
-    } else if (inf[i] == '+' || inf[i] == '-' || inf[i] == '*' || inf[i] == '/') {
-      while (!opStack.isEmpty() && getPriority(opStack.peek()) >= getPriority(inf[i])) {
-        result += opStack.pop();
-        result += ' ';
+    } else if (inf[i] == '+' || inf[i] == '-' ||
+               inf[i] == '*' || inf[i] == '/') {
+      while (!opStack.isEmpty() &&
+             getPriority(opStack.peek()) >= getPriority(inf[i])) {
+        res += opStack.pop();
+        res += ' ';
       }
       opStack.push(inf[i]);
       i++;
@@ -43,41 +44,38 @@ std::string infx2pstfx(const std::string& inf) {
   }
 
   while (!opStack.isEmpty()) {
-    result += opStack.pop();
-    result += ' ';
+    res += opStack.pop();
+    res += ' ';
   }
 
-  return result;
+  return res;
 }
 
 int eval(const std::string& post) {
-  TStack<int, 100> valStack;
+  TStack<int, 100> stack;
   int i = 0;
 
-  while (i < post.size()) {
+  while (i < post.length()) {
     if (std::isdigit(post[i])) {
-      int number = 0;
-      while (i < post.size() && std::isdigit(post[i])) {
-        number = number * 10 + (post[i] - '0');
+      int num = 0;
+      while (i < post.length() && std::isdigit(post[i])) {
+        num = num * 10 + (post[i] - '0');
         i++;
       }
-      valStack.push(number);
-    } else if (post[i] == '+' || post[i] == '-' || post[i] == '*' || post[i] == '/') {
-      int b = valStack.pop();
-      int a = valStack.pop();
-      int res = 0;
-      switch (post[i]) {
-        case '+': res = a + b; break;
-        case '-': res = a - b; break;
-        case '*': res = a * b; break;
-        case '/': res = a / b; break;
-      }
-      valStack.push(res);
+      stack.push(num);
+    } else if (post[i] == '+' || post[i] == '-' ||
+               post[i] == '*' || post[i] == '/') {
+      int b = stack.pop();
+      int a = stack.pop();
+      if (post[i] == '+') stack.push(a + b);
+      else if (post[i] == '-') stack.push(a - b);
+      else if (post[i] == '*') stack.push(a * b);
+      else if (post[i] == '/') stack.push(a / b);
       i++;
     } else {
       i++;
     }
   }
 
-  return valStack.pop();
+  return stack.pop();
 }
